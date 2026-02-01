9669cff68fd65432fa6665316cba5c19a5c012d8
Mova del Sol <47517248+movadelsol@users.noreply.github.com>
2026-02-01 21:05:38 +0900

    *_spec.rb modified again

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,25 +1,29 @@
-# require_relative '../fizzbuzz' # fizzbuzz.rb を読み込む
+require_relative '../fizzbuzz'
 
-RSpec.describe "Fizzbuzz" do
-  # テスト対象のインスタンスを生成（fizzbuzz.rbにFizzbuzzクラスがある場合）
-  let(:fizzbuzz) { Fizzbuzz.new }
+RSpec.describe Fizzbuzz do
+  # subject に Fizzbuzz.new を割り当てることで、テスト内で subject が使えるようになります
+  subject { Fizzbuzz.new }
 
   context 'normal number' do
-    it { expect(fizzbuzz.calculate(1)).to eq 1 }
-    it { expect(fizzbuzz.calculate(2)).to eq 2 }
+    it { expect(subject.calculate(1)).to eq 1 }
+    it { expect(subject.calculate(2)).to eq 2 }
   end
 
   context 'Fizz number' do
-    it { expect(fizzbuzz.calculate(3)).to eq 'fizz' }
-    it { expect(fizzbuzz.calculate(6)).to eq 'fizz' }
+    it { expect(subject.calculate(3)).to eq 'fizz' }
   end
 
   context 'Buzz number' do
-    it { expect(fizzbuzz.calculate(5)).to eq 'buzz' }
-    it { expect(fizzbuzz.calculate(10)).to eq 'buzz' }
+    it { expect(subject.calculate(5)).to eq 'buzz' }
   end
 
   context 'FizzBuzz number' do
-    it { expect(fizzbuzz.calculate(15)).to eq 'fizzbuzz' }
+    it { expect(subject.calculate(15)).to eq 'fizzbuzz' }
+  end
+
+  # せっかくなので「GitHub」のテストも追加しましょう！
+  context 'Special number' do
+    it { expect(subject.calculate(7)).to eq 'GitHub' }
+    it { expect(subject.calculate(17)).to eq 'GitHub' }
   end
 end
